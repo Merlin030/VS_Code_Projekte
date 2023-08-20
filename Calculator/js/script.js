547e7885ca120419863b08e3b982bae29f8a5f57
Merlin030 <142410994+Merlin030@users.noreply.github.com>
2023-08-20 23:50:37 +0200

    Update script.js

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -1,4 +1,4 @@
   function openPopup() 
     {
-        window.open('calculator.html', 'calculator', 'height=400,width=400,resizable=yes,location=no,menubar=no,toolbar=no,status=no,scrollbars');
+        window.open('calculator.html', 'calculator', 'height=400,width=400,resizable=yes,location=no,menubar=no,toolbar=no,status=no,scrollbars=no');
     }
